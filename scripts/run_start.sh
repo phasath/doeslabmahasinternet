da7bddc655296d8cf4bcdbebef1fce5a0f6268ad
Raphael Sathler <sathler93@gmail.com>
2019-12-26 02:32:34 -0300

    Fixed wrong env name

--- scripts/run_start.sh
+++ scripts/run_start.sh
@@ -3,4 +3,4 @@ export HOST=0.0.0.0
 export PORT=5000
 export AIO_ROOT=$(pwd)/app
 export PYTHONPATH=$(pwd)/app
-gunicorn autoapp:web_app -b ${AIO_HOST}:${AIO_PORT} --chdir app --workers 4 --worker-class aiohttp.worker.GunicornWebWorker
+gunicorn autoapp:web_app -b ${HOST}:${PORT} --chdir app --workers 4 --worker-class aiohttp.worker.GunicornWebWorker
